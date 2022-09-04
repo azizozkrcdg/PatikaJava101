09b0e02059f08caae3d936c16c1dcd09362da2ae
Muhammed Aziz Özkaracadağ <98597050+azizozkrcdg@users.noreply.github.com>
2022-09-04 20:23:13 +0300

    Add files via upload

---
+++ usAlma.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class usAlma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayi, kuvvet, total = 1;
+
+        System.out.print("Kuvveti Alıancak Sayıyı Giriniz : "  );
+        sayi = input.nextInt();
+
+        System.out.print("Kuvvet Giriniz : ");
+        kuvvet = input.nextInt();
+
+        for (int i = 1; i <= kuvvet; i++){
+            total *= sayi;
+        }
+        System.out.print(total);
+
+
+    }
+}
