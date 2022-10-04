2760090ccab576dc9f7fa732b2c4ef3743141145
Muhammed Aziz Özkaracadağ <98597050+azizozkrcdg@users.noreply.github.com>
2022-10-04 19:06:51 +0300

    Add files via upload

---
+++ vucutKitleIndeksi/src/kitleIndeksi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class kitleIndeksi {
+    public static void main(String[] args) {
+
+        // değişkenleri tanımla
+        float boy, kilo, indeks;
+
+        // scanner tanımla
+        Scanner girdi = new Scanner(System.in);
+
+        // kullanıcıdan bilgi al
+        System.out.print("Lütfen Boyunuzu Metre Cinsinden Giriniz : ");
+        boy = girdi.nextFloat();
+
+        System.out.print("Lütfen Kilonuzu  Cinsinden Giriniz : ");
+        kilo = girdi.nextFloat();
+
+        // formül
+        indeks = kilo / (boy*boy);
+
+        // ekrana yazdır
+        System.out.print("Vücut kitle indeksiniz : " + indeks);
+
+    }
+}
