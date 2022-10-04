6aee415ab0dbcc298407da662929e33bbcbf45f1
Muhammed Aziz Özkaracadağ <98597050+azizozkrcdg@users.noreply.github.com>
2022-10-04 18:44:56 +0300

    Add files via upload

---
+++ YuzeKadarOlanAsalSayilar/src/yuzeKadarOlanAsalSayilar.java
@@ -0,0 +1,19 @@
+public class yuzeKadarOlanAsalSayilar {
+    public static void main(String[] args) {
+
+        String asalSayilar = "";
+        for (int i = 1; i <= 100; i++) {
+            int sayac = 0;
+            for (int j = i; j >= 1; j--) {
+                if (i % j == 0) {
+                    sayac ++;
+                }
+            }
+            if (sayac == 2) {
+                asalSayilar = asalSayilar + i + " ";
+            }
+        }
+        System.out.println(asalSayilar);
+
+    }
+}
